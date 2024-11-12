43f54e8765071929503d1da470a5bbdae9a84237
Juan Esteban <150542658+estebaaam@users.noreply.github.com>
2024-11-11 20:27:59 -0500

    depurando detalles de la tienda, pagina about us añadida

--- js/buscador.js
+++ js/buscador.js
@@ -17,23 +17,33 @@ if(!busquedaUsuario){
   if(productosFiltrados.length > 0){
     let productosHTML = '';
     let stockText = '';
-    productosHTML += `<h3 my-5>numero de coincidencias: ${coincidencias}</h3>`;
+    productosHTML += `<h3>numero de coincidencias: ${coincidencias}</h3>`;
     productosFiltrados.forEach(producto => {
       stockText = producto.existencias ? "En stock" : "Agotado";
       productosHTML += `
-    <div class="product-section">
-      <div class="product-image-container">
-        <img class="product-img" src="${producto.imagen}">
+    <section class="item-details section">
+    <div class="container">
+        <div class="top-area">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12 col-12">
+                    <div class="product-image">
+                            <div class="main-img">
+                                <img src="${producto.imagen}" id="product-img">
+                            </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-12">
+                    <div class="product-info">
+                        <a href="../html/product-details.html" onclick="validarIdProducto(${producto.id})"><h2 id="product-name" class="title">${producto.nombre}</h2></a>
+                        <h2 id="stock" class="title">${stockText}</h2>
+                        <h3 id="price" class="price text-danger">$${producto.precio}</h3>
+                        <p id="description" class="info-text">${producto.descripcion}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="info-section">
-        <a href="../html/product-details.html" onclick="validarIdProducto(${producto.id})">
-          <h3 class="product-title">${producto.nombre}</h3>
-        </a>
-          <p class="price">$${producto.precio}</p>
-          <p class="stock">${stockText}</p>
-          <p class="product-description">${producto.descripcion}</p>
-          </div>
     </div>
+</section>
     `
     });
     document.querySelector('.cart-wrapper').innerHTML = productosHTML;
