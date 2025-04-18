f7ad581d95bc840909ea45b41700d84d1b13fe26
juan esteban <jcoobex@gmail.com>
2025-04-18 16:49:35 -0500

    correct the database connection url

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/e commerce"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/e_commerce"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
